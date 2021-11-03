a225302f5a513e45947dfc5994866c50b956f8a8
Lee <497232807@qq.com>
2021-11-03 10:29:35 +0800

    登录

--- src/router/router.js
+++ src/router/router.js
@@ -2,19 +2,20 @@
  * @Description: 
  * @Author: li qiang
  * @Date: 2021-10-20 16:09:50
- * @LastEditTime: 2021-10-20 16:09:50
+ * @LastEditTime: 2021-11-03 10:19:56
  */
 
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import HelloWorld from '../components/HelloWorld.vue'
-import Test from '../components/Test.vue'
+import Login from '../view/login/Login.vue'
+// import Test from '../components/Test.vue'
 Vue.use(VueRouter)
 
 const routes = [
-    { path: '/', component: HelloWorld },
-    { path: "/test", component: Test }
+    { path: '/', component: Login },
+    { path: "/test", component: HelloWorld }
 
 ]
 var router= new VueRouter({
