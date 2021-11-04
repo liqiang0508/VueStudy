1aa9bf9299308371055730757c616fffb559ec2c
Lee <497232807@qq.com>
2021-11-04 12:12:13 +0800

    根据当前浏览器语言

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: li qiang
  * @Date: 2021-11-03 14:18:02
- * @LastEditTime: 2021-11-03 14:42:37
+ * @LastEditTime: 2021-11-04 11:53:22
  */
 
 //i18n
@@ -16,10 +16,15 @@ const messages = {
     en: en,
     zh: zh
 }
-
-
+//浏览器语言
+// http://www.lingoes.net/zh/translator/langcode.htm
+let lang = navigator.language
+let langCode = lang.substr(0, 2)
+if (messages[langCode] === undefined ){
+    langCode = 'zh'
+}
 const i18n = new VueI18n({
-    locale: 'zh', // 设置语言环境
+    locale: langCode, // 设置语言环境
     messages
 })
 
